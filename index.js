9da88df5e40083294ca3755300bb65775fd081e7
Aria Stewart <aredridel@dinhe.net>
2015-09-05 13:41:43 -0400

    Promisify

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var P = require('bluebird');
+var fs = P.promisifyAll(require('fs'));
 var https = require('https');
 var path = require('path');
+var VError = require('verror');
 
 var p = parse(process.argv[2]);
 var version = process.argv[3];
@@ -13,8 +15,7 @@ if (!p.platform || !p.arch || !version || !p.product) {
     return;
 }
 
-
-function go(platform, arch, version, product, cb) {
+function go(platform, arch, version, product) {
     var dir = product + "-" + platform + '-' + arch;
     var base = product + "-v" + version + "-" + platform + "-" + arch;
     var filename = base + ".tar.gz";
@@ -42,31 +43,33 @@ function go(platform, arch, version, product, cb) {
         package.bin.iojs = path.join(base, "bin/iojs");
     }
 
-    fs.mkdir(dir, function (err) {
+    return fs.mkdirAsync(dir).catch(function(err) {
         if (err && err.code != 'EEXIST') {
-            return cb(err);
+            throw err;
         }
-        var req = https.get({hostname: (product == "iojs" ? "iojs.org" : "nodejs.org"), path: (/rc/.test(version) ? "/download/rc/v" : "/dist/v") + version + "/" + filename});
-        req.on('error', cb);
-        req.on('response', function (res) {
-            if (res.statusCode != 200) return cb("not ok: " + res.statusCode);
+    }).then(function () {
+        return new P(function (accept, reject) {
+            var spec = {hostname: (product == "iojs" ? "iojs.org" : "nodejs.org"), path: (/rc/.test(version) ? "/download/rc/v" : "/dist/v") + version + "/" + filename}
+            var req = https.get(spec);
+            req.on('error', reject);
+            req.on('response', function (res) {
+                if (res.statusCode != 200) return reject(new VError("not ok: fetching %j got status code %s",  spec, res.statusCode));
 
-            res.pipe(fs.createWriteStream(path.join(dir, filename))).on('error', function (err) {
-                return cb(err);
-            }).on('finish', function () {
-                fs.writeFile(path.join(dir, 'package.json'), JSON.stringify(package), function (err) {
-                    return cb(err);
+                res.pipe(fs.createWriteStream(path.join(dir, filename))).on('error', function (err) {
+                    reject(err);
+                }).on('finish', function () {
+                    accept();
                 });
             });
         });
+    }).then(function () {
+        return fs.writeFile(path.join(dir, 'package.json'), JSON.stringify(package));
     });
 }
 
-go(p.platform, p.arch, version, p.product, function (err) {
-    if (err) {
-        console.warn(err);
-        process.exit(1);
-    }
+go(p.platform, p.arch, version, p.product).catch(function (err) {
+    console.warn(err);
+    process.exit(1);
 });
 
 function parse(str) {
